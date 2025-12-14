7839a40df7ae9483060f76d39d1a1a38079ac1cc
Andreas Öhlund <andreas.ohlund@particular.net>
2025-12-14 15:07:32 +0100

    Add skeleton

---
+++ src/AzureBlobTransport.TransportTests/ConfigureAzureBlobTransportInfrastructure.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using NServiceBus.Transport;
+using NServiceBus.TransportTests;
+
+class ConfigureAzureBlobTransportInfrastructure : IConfigureTransportInfrastructure
+{
+    public TransportDefinition CreateTransportDefinition()
+    {
+        var connectionString = Environment.GetEnvironmentVariable("AzureBlobTransport_ConnectionString");
+
+        return null;
+    }
+
+    public async Task<TransportInfrastructure> Configure(TransportDefinition transportDefinition, HostSettings hostSettings, QueueAddress inputQueue, string errorQueueName, CancellationToken cancellationToken = default)
+    {
+        var mainReceiverSettings = new ReceiveSettings(
+            "mainReceiver",
+            inputQueue,
+            true,
+            false,
+            errorQueueName);
+
+        var transport = await transportDefinition.Initialize(hostSettings, [mainReceiverSettings], [errorQueueName], cancellationToken);
+
+        queuesToCleanUp = [transport.ToTransportAddress(inputQueue), errorQueueName];
+        return transport;
+    }
+
+    public Task Cleanup(CancellationToken cancellationToken = default)
+    {
+        return Task.CompletedTask;
+    }
+
+    string[] queuesToCleanUp;
+}
